b66e14075d6308fe42a8f06a60913e69aa294a8b
ozkancondek <ozkncndek@gmail.com>
2022-04-18 19:47:19 +0200

    add components to pages

---
+++ net_ninja/ninjalist/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <p> Copyrights 2022 @ozkan</p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ net_ninja/ninjalist/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="logo">
+        <h1>Navbar Component</h1>
+      </div>
+      <a> Home</a>
+      <a> About</a>
+      <a> Listing</a>
+    </nav>
+  );
+};
+
+export default Navbar;
