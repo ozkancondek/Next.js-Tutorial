58083a7abd287804a8a0c0c847f0ab1ab8877ced
ozkancondek <ozkncndek@gmail.com>
2022-04-19 16:57:03 +0200

    crating layout component and warapping children

--- net_ninja/ninjalist/pages/index.js
+++ net_ninja/ninjalist/pages/index.js
@@ -1,10 +1,6 @@
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
-
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>homepage</h1>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae, dolor!
@@ -12,7 +8,6 @@ export default function Home() {
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae, dolor!
       </p>
-      <Footer />
     </div>
   );
 }
