15e4de206ce1ee31ef3261a51dcb0b103f5d5d39
ozkancondek <ozkncndek@gmail.com>
2022-04-19 15:57:51 +0200

    Linking Pages

--- net_ninja/ninjalist/components/Navbar.js
+++ net_ninja/ninjalist/components/Navbar.js
@@ -1,12 +1,20 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
         <h1>Navbar Component</h1>
       </div>
-      <a> Home</a>
-      <a> About</a>
-      <a> Listing</a>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/ozkan">
+        <a>ozkan page</a>
+      </Link>
     </nav>
   );
 };
