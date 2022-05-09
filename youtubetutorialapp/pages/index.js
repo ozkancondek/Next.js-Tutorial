953fbd8170205fdef7370fba835fe9e846364a5e
ozkancondek <ozkncndek@gmail.com>
2022-05-09 16:13:32 +0200

    File structure,error page, basic routing

---
+++ youtubetutorialapp/pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Next Project Youtube</h1>
+      <h2>Main Page</h2>
+    </div>
+  );
+}
---
+++ youtubetutorialapp/pages/users/users.js
@@ -0,0 +1,9 @@
+const Users = () => {
+  return (
+    <div>
+      <h2>this is users page</h2>
+    </div>
+  );
+};
+
+export default Users;
